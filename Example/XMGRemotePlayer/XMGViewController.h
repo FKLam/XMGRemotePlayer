9272bef5110e1ff99825a1d195ebe55d322f2e46
kun <lfkun@aoyuan.net>
2017-08-07 23:02:18 +0800

    Initial commit

---
+++ Example/XMGRemotePlayer/XMGViewController.h
@@ -0,0 +1,13 @@
+//
+//  XMGViewController.h
+//  XMGRemotePlayer
+//
+//  Created by lfk0120@163.com on 08/07/2017.
+//  Copyright (c) 2017 lfk0120@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XMGViewController : UIViewController
+
+@end
---
+++ Example/XMGRemotePlayer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XMGRemotePlayer
+//
+//  Created by lfk0120@163.com on 08/07/2017.
+//  Copyright (c) 2017 lfk0120@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "XMGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XMGAppDelegate class]));
+    }
+}
